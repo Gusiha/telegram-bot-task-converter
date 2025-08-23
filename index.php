82c780884931f8ac82b7433ef342c6c2f15f730d
Timur Milushev <t.milushev@fitmost.ru>
2025-08-23 22:58:43 +0300

    убрал коммент

--- index.php
+++ index.php
@@ -35,6 +35,3 @@ $bot->fallback(function (Nutgram $bot) {
 });
 
 $bot->run();
-
-
-https://youtrack.mostfit.ru/tickets/HELP-2437/Pochinit-vygruzku-studij-iz-srm-na-pauze проблемы не оказалось, все нормально экспортируется, скинул excel с таблицами на паузе https://youtrack.mostfit.ru/tickets/HELP-2442/Nuzhna-uchetka-data-grip решена https://youtrack.mostfit.ru/tickets/HELP-2167/Integraciya-sycret-rasshirit-zagruzhaemye-okna зависшая задачка в бэклоге, тегнул Игоря https://youtrack.mostfit.ru/tickets/HELP-2436/Ne-otobrazhayutsya-dostupnye-sloty-minimalnogo-vremeni-bronirovaniya проверил, что сейчас слоты появляются меньше чем за 60 минут, жду ответа https://youtrack.mostfit.ru/tickets/HELP-2437/Pochinit-vygruzku-studij-iz-srm-na-pauze проблемы не оказалось, все нормально экспортируется, скинул excel с таблицами на паузе https://youtrack.mostfit.ru/tickets/HELP-2451/Proverit-na-frod проверил существует ли антифрод для смс, спросил у олдов это все старая история, Арина должна сама настроить это в МТС https://youtrack.mostfit.ru/tickets/HELP-2449/Otklyuchenie-studii-ot-integracii-s-mobifitness связано с mobFit, тегнул Никиту, он объяснил как там все удалить, чтобы потом заработало, написал sql-запрос, сохранил, тикет закрыл https://youtrack.mostfit.ru/tickets/HELP-2422/Ne-poluchaetsya-zapisatsya-v-Reshape попытался записаться, все нормально отрабатывает, жду ответа
